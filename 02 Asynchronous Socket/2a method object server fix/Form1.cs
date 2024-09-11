15b893322cc3facda9c19476739c49af9369fb53
nandamail <nandapens30@gmail.com>
2024-09-11 09:39:46 +0700

    update

---
+++ 02 Asynchronous Socket/2a method object server fix/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class Form1 : Form
+    {
+        private Pen red = new Pen(Color.Red);
+        private Rectangle rect = new Rectangle(20, 20, 30, 30);
+        private SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        private int slide = 10;
+
+        private TcpListener listener;
+        private List<TcpClient> clients = new List<TcpClient>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+
+            StartServer();
+        }
+
+        private void StartServer()
+        {
+            listener = new TcpListener(System.Net.IPAddress.Any, 5000);
+            listener.Start();
+
+            Task.Run(() =>
+            {
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    lock (clients)
+                    {
+                        clients.Add(client);
+                    }
+                }
+            });
+        }
+
+        private void SendDataToClients()
+        {
+            string dataToSend = $"{rect.X},{rect.Y},{rect.Width},{rect.Height}";
+            byte[] bytesToSend = Encoding.ASCII.GetBytes(dataToSend);
+
+            lock (clients)
+            {
+                foreach (var client in clients)
+                {
+                    NetworkStream stream = client.GetStream();
+                    stream.Write(bytesToSend, 0, bytesToSend.Length);
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            back();
+
+            rect.X += slide;
+            Invalidate();
+
+            SendDataToClients(); // Kirim data ke client
+        }
+
+        private void back()
+        {
+            if (rect.X >= this.Width - rect.Width * 2)
+                slide = -10;
+            else if (rect.X <= rect.Width / 2)
+                slide = 10;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+    }
+}
---
+++ 02 Asynchronous Socket/2b2 method object client fix/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MovingObject
+{
+    public partial class Form1 : Form
+    {
+        private Pen red = new Pen(Color.Red);
+        private Rectangle rect = new Rectangle(20, 20, 30, 30);
+        private SolidBrush fillBlue = new SolidBrush(Color.Blue);
+        private TcpClient client;
+        private NetworkStream stream;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            if (!ConnectToServer())
+            {
+                MessageBox.Show("Unable to connect to server. The application will now exit.");
+                Application.Exit(); // Keluar dari aplikasi jika koneksi gagal
+            }
+
+            timer1.Interval = 50;
+            timer1.Enabled = true;
+        }
+
+        private bool ConnectToServer()
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.2", 5000); // IP dan port server
+                stream = client.GetStream();
+                return true; // Koneksi berhasil
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error connecting to server: " + ex.Message);
+                return false; // Koneksi gagal
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ReceiveData();
+            Invalidate();
+        }
+
+        private void ReceiveData()
+        {
+            if (stream != null && stream.DataAvailable)
+            {
+                byte[] data = new byte[1024];
+                int bytesRead = stream.Read(data, 0, data.Length);
+                string receivedData = Encoding.ASCII.GetString(data, 0, bytesRead);
+
+                // Parsing posisi rectangle dari string
+                string[] parts = receivedData.Split(',');
+                if (parts.Length == 4)
+                {
+                    int x = int.Parse(parts[0]);
+                    int y = int.Parse(parts[1]);
+                    int width = int.Parse(parts[2]);
+                    int height = int.Parse(parts[3]);
+
+                    rect = new Rectangle(x, y, width, height);
+                }
+            }
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.DrawRectangle(red, rect);
+            g.FillRectangle(fillBlue, rect);
+        }
+    }
+}
